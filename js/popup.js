9ae457435e643a18c8df42584771d8f942677c19
Keshav <xkeshav@gmail.com>
2014-12-04 17:43:21 +0530

    highlight argument removed

--- js/popup.js
+++ js/popup.js
@@ -2,14 +2,14 @@ debugger;
 
 function click(e) {
   var cl = e.target.className; // this.className both gives the same result that is OK.
-  chrome.tabs.query({ active: true, highlighted: true, currentWindow: true }, function(htab) {
-    console.log(JSON.stringify(htab, ['active', 'id', 'index', 'windowId', 'title', 'url'], 4));
-      chrome.tabs.executeScript(htab[0].id, {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(thisTab) {
+    console.log(JSON.stringify(thisTab, ['active', 'id', 'index', 'windowId', 'title', 'url'], 4));
+      chrome.tabs.executeScript(thisTab[0].id, {
         code : "var scriptOptions = { selectedClass:" + JSON.stringify(cl) + " }" }, function() {
           /* NOTE: change script.js with jscript.js  and see jquery code working */
-          chrome.tabs.executeScript(htab[0].id, { file: "js/script.js" }, function(){
+          chrome.tabs.executeScript(thisTab[0].id, { file: "js/script.js" }, function(){
             console.log('sun raha hai na tun!');
-            chrome.tabs.sendMessage(htab[0].id, { action: "bodyColor" }, function(resp) {
+            chrome.tabs.sendMessage(thisTab[0].id, { action: "bodyColor" }, function(resp) {
               console.log('fir se ' + resp.farewell);
             }); // !-- tabs.sendMessage
           }); // !-- INNER tabs.execeuteScripts
